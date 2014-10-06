bd7fc0f7a4086f7c0a7958ca63cc582a99c96225
ChengyuanMa <chengyuan@leandatainc.com>
2014-10-06 08:57:31 -0700

    Finish staatic pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 SampleApp::Application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  get "static_pages/home"
+  get "static_pages/help"
+  get "static_pages/about"
+  
+  
+ 
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
